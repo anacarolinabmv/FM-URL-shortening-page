33feeb874ba7a377dd570495a83f71455b06a14e
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-09-03 20:16:28 +0200

    Implemented render shortened link and copy link btn

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const inputEl = document.getElementById('input-link');
 const btnShorten = document.getElementById('btn-shorten');
+let btnCopy;
+const shortLinkContainer = document.querySelector('.shortened-link__container');
 
 const shortenLink = async function () {
   try {
@@ -10,13 +12,37 @@ const shortenLink = async function () {
 
     const response = await fetch(apiUrl);
     const data = await response.json();
+    const shortUrl = await data.result.short_link;
 
     if (!data.ok) throw new Error('Could not shorten link! ');
 
-    console.log(data.result.short_link);
+    renderLink(shortUrl, longUrl);
   } catch (err) {
     console.log(err);
   }
+};
+
+const copyLink = function (btn) {
+  // const a = btn.closest('.short-link__box');
+};
+const renderLink = function (shortUrl, longUrl) {
+  const html = `  <div class="shortened-link__box">
+            <a href="${longUrl}" class="long-link">${longUrl}</a>
+            <div class="short-link__box">
+              <a href="https://${shortUrl}" class="short-link">${shortUrl}</a>
+              <a href="#" class="btn copy-link__btn font-bold" >Copy</a>
+            </div>
+          </div>`;
+
+  shortLinkContainer.insertAdjacentHTML('afterbegin', html);
+
+  btnCopy = document.querySelector('.copy-link__btn');
+
+  btnCopy.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    navigator.clipboard.writeText(shortUrl);
+  });
 };
 
 btnShorten.addEventListener('click', (event) => {
